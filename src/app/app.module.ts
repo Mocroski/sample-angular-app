ae4301d80719df8305efd0236938be800febd765
LukeMocroski <devmocroski@gmail.com>
2023-03-26 10:06:09 -0300

    Estuturando os componentes do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { RecipeListComponent } from './header/recipe-list/recipe-list.component';
+import { RecipesComponent } from './header/recipes/recipes.component';
+import { RecipeDetailComponent } from './header/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './header/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
