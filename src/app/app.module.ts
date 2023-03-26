77e937c34690ddcae2a0de811b2ccddd7316920a
LukeMocroski <devmocroski@gmail.com>
2023-03-26 11:19:19 -0300

    Comecando a estilizar os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
-import { RecipeListComponent } from './header/recipe-list/recipe-list.component';
-import { RecipesComponent } from './header/recipes/recipes.component';
-import { RecipeDetailComponent } from './header/recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './header/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    RecipeListComponent,
     RecipesComponent,
     RecipeDetailComponent,
+    RecipeListComponent,
     RecipeItemComponent,
+    ShoppingListComponent,
     ShoppingEditComponent
+
   ],
   imports: [
     BrowserModule
